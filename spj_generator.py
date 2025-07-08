5011a78a4d44c6bfc55496ff6ca7d336b6af8aa0
gamboel99 <eko.w.a@students.kahuripan.ac.id>
2025-07-08 19:06:44 +0700

    Update spj_generator.py

--- spj_generator.py
+++ spj_generator.py
@@ -1,9 +1,19 @@
 import os
+import pkgutil
 from docx import Document
+import io
 
 def buat_spj(lembaga, nama_kegiatan, tgl, lokasi, anggaran, realisasi, sumber_dana, bukti_upload):
-    template_path = os.path.join(os.path.dirname(__file__), "templates", "template_spj.docx")
-    doc = Document(template_path)
+    # Baca file template dari folder templates/ sebagai binary
+    template_bytes = pkgutil.get_data(__name__, "templates/template_spj.docx")
+    
+    if not template_bytes:
+        raise FileNotFoundError("template_spj.docx tidak ditemukan")
+
+    # Load dokumen dari bytes
+    doc = Document(io.BytesIO(template_bytes))
+
+    # Simpan hasilnya
     out_path = os.path.join(os.path.dirname(__file__), "SPJ_Kegiatan.docx")
     doc.save(out_path)
     return out_path
